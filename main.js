778e4af63e17f5b14cea64303c9394ffeccdf010
ayu345-are <anggraeniayu066@gmail.com>
2026-09-13 20:49:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ chatForm.addEventListener("submit", async (event) => {
 })
 
 // fitur pesan Listener (Realtime)
-const queryPesan = query(messageCollection, orderBy("waktu", "asc"))
+const queryPesan = query(messagesCollection, orderBy("waktu", "asc"))
 
 onSnapshot(queryPesan, (snapshot) => {
 
